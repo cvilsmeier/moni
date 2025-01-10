f66e737a1010dd5fa9b190786f6faa5584d77200
Christoph Vilsmeier <cv@vilsmeier-consulting.de>
2025-01-10 15:12:57 +0100

    github build

--- clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -e
-stat go.mod > /dev/null # must be in src dir
-
-rm -rf _dist
--- scripts/dist.sh
+++ scripts/dist.sh
@@ -11,6 +11,6 @@ CGO_ENABLED=0 GOOS=linux   GOARCH=amd64 go build -o _dist/linux .
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o _dist/windows .
 
 echo "tar"
-tar -czf _dist/linux/moni-linux-amd64.tar.gz     README.md -C _dist/linux moni
-tar -czf _dist/windows/moni-windows-amd64.tar.gz README.md -C _dist/windows moni.exe
+tar -czf _dist/moni-linux.tar.gz   README.md -C _dist/linux   moni
+tar -czf _dist/moni-windows.tar.gz README.md -C _dist/windows moni.exe
 
