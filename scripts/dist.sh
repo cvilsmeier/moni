fe616669b60e4fcd343ed89e4415d1d4b5ed2b4f
Christoph Vilsmeier <cv@vilsmeier-consulting.de>
2025-01-10 22:31:43 +0100

    github build

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -11,6 +11,6 @@ CGO_ENABLED=0 GOOS=linux   GOARCH=amd64 go build -o _dist/linux .
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o _dist/windows .
 
 echo "tar"
-tar -czf _dist/moni-linux.tar.gz   README.md -C _dist/linux   moni
-tar -czf _dist/moni-windows.tar.gz README.md -C _dist/windows moni.exe
+tar -czf _dist/moni-linux.tar.gz  -C _dist/linux   moni
+zip -j _dist/moni-windows.zip  _dist/windows/moni.exe README.md
 
